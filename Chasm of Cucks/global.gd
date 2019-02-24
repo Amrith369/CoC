4a01adea50d700a44cd55f20ede7e2a88b435acb
Amrith <mark.siminski@connect.kwoods.org>
2019-02-23 20:06:04 -0500

    Menu-Changes
    ....

--- Chasm of Cucks/global.gd
+++ Chasm of Cucks/global.gd
@@ -4,10 +4,13 @@ extends Node
 var currentChar = "null"
 var currentLives = 4
 var currentHealth = 10
+var achievements = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
 
 func _ready():
 	#var root = get_tree().get_root()
 	#current_scene = root.get_child(root.get_child_count() -1)
+	
+	print(achievements)
 	pass
 #func _process(delta):
 #	print(currentChar)
